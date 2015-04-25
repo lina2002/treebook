c95bba79853667fdcd4970801624a4a9b27cc359
Agnieszka Paszek <lina2002@go2.pl>
2015-04-26 00:21:58 +0200

    Sign-up form changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+  	conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+    	where(conditions.to_hash).where(["lower(profile_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+    	where(conditions.to_hash).first
+    end
+  end
+
+  validates :profile_name,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
 end
